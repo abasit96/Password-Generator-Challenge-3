ab1ab57f6bfe6d1022130944b5c2110c85feefa7
Bootcamp <bootcamp@Abduls-MacBook-Air.local>
2023-01-14 13:23:59 -0700

    added code to password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,8 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   var passwordLength = 12;
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
