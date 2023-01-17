3828a3f480306b36df72ec2e67d476a2da0c08d2
Bootcamp <bootcamp@Abduls-MacBook-Air.local>
2023-01-17 08:32:27 -0700

    Worked with my tutor to generate a password, currently working on how to add a minimum and maximum password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,48 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C","D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q","R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+var selected = []
 // Write password to the #password input
+function generatePassword () {
+  selected = []
+  var passwordLength = prompt("What do you want to be the password length?");
+  var includeLower = confirm("Do you want to include lower case?");
+  var includeUpperCase = confirm("Do you want to include upper case letter?");
+  var includeNumbers = confirm("Do you want to include number?");
+  var includeSpecial = confirm("Do you want to include special characters?");
+
+  console.log(passwordLength);
+
+  console.log([includeLower, includeUpperCase, includeNumbers, includeSpecial]);
+
+  if (includeLower == true) {
+    selected = [...selected, ...lowerCase]
+  }
+  if (includeUpperCase == true) {
+    selected = [...selected, ...upperCase]
+  }
+  if (includeNumbers == true) {
+    selected = [...selected, ...numbers]
+  }
+  if (includeSpecial == true) {
+    selected = [...selected, ...special]
+  }
+  console.log(selected)
+  
+  var password = "";
+
+  for(i = 0; i < passwordLength; i++) {
+    var randomChar = selected [Math.floor (Math.random () * selected.length)];
+
+    password = password + randomChar;
+  }
+
+  return password;
+}
 function writePassword() {
-  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-   var passwordLength = 12;
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
